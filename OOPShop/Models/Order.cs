c89ce3907496932a8f01b3a9b4fd8f6f90cc502b
nikita <nikitamazurenko42@gmail.com>
2022-12-13 21:09:00 +0200

    Add project files.

---
+++ OOPShop/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace OOPShop.Models
+{
+    public enum OrderStatus
+    {
+        Open, 
+        Completed, 
+        Cancelled
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public OrderStatus Status { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ OOPShop/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public Product(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
